7c720e86de1b3faccb3880af851fbaa6b8954ba2
Manuel Rivas <marivas11@gmail.com>
2012-08-28 11:41:37 +0200

    config ace-jump-mode

--- personal/conf-global-keybindings.el
+++ personal/conf-global-keybindings.el
@@ -4,3 +4,6 @@
 ;;;;(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
 
 (setq ns-right-alternate-modifier nil)
+
+(require 'ace-jump-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
--- personal/conf-packages.el
+++ personal/conf-packages.el
@@ -1,7 +1,7 @@
 ;;; Mis packages
 (require 'package)
 
-(defvar my-packages '(rvm deft))
+(defvar my-packages '(rvm deft ace-jump-mode))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
