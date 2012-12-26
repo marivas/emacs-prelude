24843f62e3daf6f098f0c7503a7020d3d4fa143b
Manuel Rivas <marivas11@gmail.com>
2012-12-26 12:23:32 +0100

    automode clojure para cljs

--- personal/conf-clojure.el
+++ personal/conf-clojure.el
@@ -1,6 +1,8 @@
 (progn
   (require 'prelude-clojure)
 
+  (add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojure-mode))
+
   (eval-after-load 'nrepl
     '(progn
        (setq nrepl-popup-stacktraces nil))))
