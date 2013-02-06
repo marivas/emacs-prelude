1e6b8fda11389e97c699cd37468279cd29efaa6f
Manuel Rivas <marivas11@gmail.com>
2013-02-06 21:12:02 +0100

    conf clojure

--- personal/conf-clojure.el
+++ personal/conf-clojure.el
@@ -1,11 +1,16 @@
-(progn
-  (require 'prelude-clojure)
+;;; conf-clojure.el --- my config de clojure
+;;; Commentary:
+;;; Code:
+(require 'prelude-clojure)
+(prelude-ensure-module-deps '(midje-mode))
 
-  (add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojure-mode))
 
-  (eval-after-load 'nrepl
-    '(progn
-       (setq nrepl-popup-stacktraces nil))))
+(add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojure-mode))
+
+(eval-after-load 'nrepl
+  '(progn
+     (setq nrepl-popup-stacktraces nil)))
 
 
 (provide 'conf-clojure)
+;;; conf-clojure.el ends here
