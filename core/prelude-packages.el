3027b35ea615dbcd36c8ed0d4d7ed2033c28b1f8
Manuel Rivas <marivas11@gmail.com>
2013-02-22 16:18:30 +0100

    add geiser repository

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -35,6 +35,9 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("geiser" . "http://mirrors.fe.up.pt/pub/nongnu/geiser/packages/") t)
+
 ;; set package-user-dir to be relative to Prelude install path
 (setq package-user-dir (expand-file-name "elpa" prelude-dir))
 (package-initialize)
