918f4bfc3866b3de9a18a28d2b8e4f9a891d683e
Manuel Rivas <marivas11@gmail.com>
2013-05-14 12:41:45 +0200

    cambio de tema y añadimos multiple-cursors

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -73,7 +73,8 @@
                                           "%b"))))
 
 ;; use zenburn as the default theme
-(load-theme 'zenburn t)
+;;(load-theme 'zenburn t)
+(load-theme 'wombat t)
 
 (provide 'prelude-ui)
 ;;; prelude-ui.el ends here
--- personal/conf-global-keybindings.el
+++ personal/conf-global-keybindings.el
@@ -1,9 +1,13 @@
 ;;; mis keybindings
+(require 'multiple-cursors)
 
+(global-set-key (kbd "C-c m m") 'mc/edit-lines)
+(global-set-key (kbd "C-c m n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c m p") 'mc/mark-previous-like-this)
 
 ;;;;(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
 
 (setq ns-right-alternate-modifier nil)
 
-(require 'ace-jump-mode)
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+;;(require 'ace-jump-mode)
+;;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
