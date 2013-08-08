247e5fdbb5d7ea0f5c3b71c14e3ec2e6593595dc
Manuel Rivas <marivas11@gmail.com>
2013-08-08 16:39:11 +0200

    fix smartparens base mode

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -102,9 +102,9 @@ Will only occur if prelude-whitespace is also enabled."
                                          try-complete-lisp-symbol))
 
 ;; smart pairing for all
-(require 'smartparens-config)
 (setq sp-base-key-bindings 'paredit)
 (setq sp-autoskip-closing-pair 'always)
+(require 'smartparens-config)
 (smartparens-global-mode +1)
 
 ;; diminish keeps the modeline tidy
