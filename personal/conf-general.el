2c9604b32d1e52cb5f8a9c1efba127627cffb1af
Manuel Rivas <marivas11@gmail.com>
2013-12-12 12:51:09 +0100

    add personal-site-conf directory

--- personal/conf-general.el
+++ personal/conf-general.el
@@ -4,3 +4,11 @@
 ;;; Desactivamos flyspell-mode
 (add-hook 'text-mode-hook 'turn-off-flyspell t)
 (add-hook 'prog-mode-hook 'turn-off-flyspell t)
+
+
+(defvar prelude-personal-site-dir (expand-file-name "site" prelude-personal-dir)
+  "This directory is for your personal configuration, that you want loaded only for this site.")
+
+(when (file-exists-p prelude-personal-dir)
+  (message "Loading site personal configuration files in %s..." prelude-personal-site-dir)
+  (mapc 'load (directory-files prelude-personal-site-dir 't "^[^#].*el$")))
--- personal/conf-ui.el
+++ personal/conf-ui.el
@@ -2,6 +2,3 @@
 
 (toggle-scroll-bar -1)
 (menu-bar-mode -1)
-
-(when window-system
-  (set-face-attribute 'default nil :font "-apple-Monaco-medium-normal-normal-*-12-*-*-*-m-0-iso8859-1"))
