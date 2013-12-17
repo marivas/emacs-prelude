04072747a8a35dd04961d26ca56b43b09508928c
marivas <marivas@mb.local>
2013-12-17 12:49:12 +0100

    desactivamos proxies

--- personal/conf-proxy.el
+++ personal/conf-proxy.el
@@ -1,8 +1,8 @@
 ;;; conf-proxy.el --- Proxy conf
-
-(setq url-proxy-services
-      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-        ("http" . "194.140.11.77:80")))
+;; estoy hay que pasarlo al sites
+;; (setq url-proxy-services
+;;       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+;;         ("http" . "194.140.11.77:80")))
 
 
 (provide 'conf-proxy)
