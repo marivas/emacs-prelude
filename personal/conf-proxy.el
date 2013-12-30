907e8eb397565636b04abd61f5d3f7e8b586a929
Manuel Rivas <marivas11@gmail.com>
2013-12-30 10:57:31 +0100

    move conf-proxy to site conf

--- personal/conf-proxy.el
+++
@@ -1,8 +0,0 @@
-;;; conf-proxy.el --- Proxy conf
-;; estoy hay que pasarlo al sites
-;; (setq url-proxy-services
-;;       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-;;         ("http" . "194.140.11.77:80")))
-
-
-(provide 'conf-proxy)
