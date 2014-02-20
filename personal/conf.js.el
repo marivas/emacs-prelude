4dbd39b1ad25a88e29f1de8d4a62e0970b63974b
Manuel Rivas <marivas11@gmail.com>
2014-02-20 18:04:41 +0100

    add tern-mode

--- personal/conf.js.el
+++ personal/conf.js.el
@@ -1,1 +1,11 @@
 (setq js-indent-level 2)
+
+;; tern
+(add-to-list 'load-path (expand-file-name "submodules/tern/emacs" prelude-dir))
+(autoload 'tern-mode "tern.el" nil t)
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+
+
+
+
+(provide 'conf-js)
