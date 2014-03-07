6ae0b4c108ac09a1d902727cf339452257d04f0f
Manuel Rivas <marivas11@gmail.com>
2014-03-07 14:55:31 +0100

    drop moz.el

--- personal/conf-js.el
+++ personal/conf-js.el
@@ -3,8 +3,7 @@
 ;; tern
 (add-to-list 'load-path (expand-file-name "submodules/tern/emacs" prelude-dir))
 (autoload 'tern-mode "tern.el" nil t)
-(add-hook 'js-mode-hook (lambda () (tern-mode t)
-                          (moz-minor-mode 1)))
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
 
 
 
