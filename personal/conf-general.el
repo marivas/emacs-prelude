a8a7918958583022ddbd1325e7dbe19d45b0518d
Manuel Rivas <marivas11@gmail.com>
2014-09-08 14:56:19 +0200

    company-cider was deprecated

--- personal/conf-general.el
+++ personal/conf-general.el
@@ -18,7 +18,7 @@
 
 
 ;; for company
-(eval-after-load 'company '(nconc company-backends '(company-cider company-inf-ruby company-tern)))
+(eval-after-load 'company '(nconc company-backends '(company-inf-ruby company-tern)))
 
 
 (provide 'conf-general)
--- personal/conf-packages.el
+++ personal/conf-packages.el
@@ -1,7 +1,7 @@
 ;;; Mis packages
 (require 'package)
 
-(defvar my-packages '(rvm deft ace-jump-mode company-cider es-mode))
+(defvar my-packages '(rvm deft ace-jump-mode  es-mode))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
