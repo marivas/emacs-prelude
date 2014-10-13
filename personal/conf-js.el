db25e4a2fc1c2dbc07ab9e39add284b7d0454864
Manuel Rivas <marivas11@gmail.com>
2014-10-13 11:28:41 +0200

    set indent spaces to 2 , and activate tern mode for js2

--- personal/conf-js.el
+++ personal/conf-js.el
@@ -1,9 +1,12 @@
 (setq js-indent-level 2)
+(setq-default js2-basic-offset 2)
+
 
 ;; tern
 (add-to-list 'load-path (expand-file-name "submodules/tern/emacs" prelude-dir))
 (autoload 'tern-mode "tern.el" nil t)
 (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
 
 
