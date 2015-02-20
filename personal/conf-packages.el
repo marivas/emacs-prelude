98afbc5cf70271e94dd6fe272e0be6a71f4abd43
Manuel Rivas <marivas11@gmail.com>
2015-02-20 13:36:51 +0100

    include ag package

--- personal/conf-packages.el
+++ personal/conf-packages.el
@@ -1,7 +1,7 @@
 ;;; Mis packages
 (require 'package)
 
-(defvar my-packages '(rvm deft ace-jump-mode  es-mode restclient multiple-cursors))
+(defvar my-packages '(rvm deft ace-jump-mode  es-mode restclient multiple-cursors ag))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
