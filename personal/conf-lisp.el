ebb4f5d7e68e4c39a03608af9b55f8ccc9e55a2f
Manuel Rivas <marivas11@gmail.com>
2015-03-13 11:55:26 +0100

    conf lisp

--- personal/conf-lisp.el
+++ personal/conf-lisp.el
@@ -1,4 +1,6 @@
 ;; Default lisp sbcl
+
 (setq slime-default-lisp 'sbcl)
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
+;(slime-require 'swank-listener-hooks)
